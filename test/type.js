b8cde01ace6173c3084621a6a0139acd36e08ba2
noffle <sww@eight45.net>
2019-06-05 13:51:25 -0700

    test: add test for ensuring only heads are returned by the type view

--- test/type.js
+++ test/type.js
@@ -49,3 +49,37 @@ test('type: get nodes', function (t) {
     })
   })
 })
+
+test('type: get nodes (heads only)', function (t) {
+  var db = createDb()
+
+  var node = {
+    type: 'node',
+    changeset: '9',
+    lat: '-11',
+    lon: '-10',
+    timestamp: '2017-10-10T19:55:08.570Z'
+  }
+
+  var node2 = {
+    type: 'node',
+    changeset: '13',
+    lat: '-14',
+    lon: '-6',
+    timestamp: '2017-12-10T19:55:08.570Z'
+  }
+
+  db.create(node, function (err, elm1) {
+    t.ifError(err)
+    db.put(elm1.id, node2, function (err, elm2) {
+      t.ifError(err)
+      collect(db.byType('node'), function (err, res) {
+        t.ifError(err)
+        console.log(res)
+        t.equal(res.length, 1)
+        t.equal(res[0].id, elm1.id)
+        t.end()
+      })
+    })
+  })
+})
